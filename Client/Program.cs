611f8e5c2def56978e85e97d3b4d147d7cba84a0
xjh2000 <934622645@qq.com>
2022-03-09 19:54:33 +0800

    change layout Icons and Style

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,28 +1,26 @@
-using AntDesign.ProLayout;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using AntDesign.ProLayout;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
-//TODO interface Icons and Style
 //TODO WPA run Logic
 //TODO optimization public file size
 
-namespace Client
+namespace Client;
+
+public class Program
 {
-    public class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddAntDesign();
-            builder.Services.Configure<ProSettings>(builder.Configuration.GetSection("ProSettings"));
+        builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+        builder.Services.AddAntDesign();
+        builder.Services.Configure<ProSettings>(builder.Configuration.GetSection("ProSettings"));
 
-            await builder.Build().RunAsync();
-        }
+        await builder.Build().RunAsync();
     }
 }
