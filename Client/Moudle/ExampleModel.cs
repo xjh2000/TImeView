d496c6bfce708c843d8ee847c679ab51f43add5c
xjh2000 <934622645@qq.com>
2022-03-10 22:45:01 +0800

    change * cloudbase enveriment add * Test page

---
+++ Client/Moudle/ExampleModel.cs
@@ -0,0 +1,9 @@
+namespace Client.Moudle;
+using System.ComponentModel.DataAnnotations;
+
+public class ExampleModel
+{
+    [Required]
+    [StringLength(10, ErrorMessage = "Name is too long.")]
+    public string? Name { get; set; }
+}
